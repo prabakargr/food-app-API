edc49ff676ed44ec00dc076dec2e3be8ec38ab44
Jagan Raja <jaganraja479@gmail.com>
2018-03-07 12:32:22 +0530

    customer api updation

--- customer/customersController.js
+++ customer/customersController.js
@@ -1,5 +1,16 @@
 var customerModel=require('./customerModel');
 
+var custPreference=function(req,res){
+    var custsPref=new customerModel(req.body);
+    custsPref.save(function(err,pref){
+        if(!err){
+            res.status(200).send(pref)
+        }else{
+            res.status(500).send('cannot send');
+        }
+    })
+}
+
 module.exports={
-    
+    custPreference:custPreference,
 }
--- customer/customersRouting.js
+++ customer/customersRouting.js
@@ -4,5 +4,7 @@ var customersRouting=express.Router();
 
 var customersController=require('./customersController');
 
+customersRouting.route('/customerPreference').post(customersController.custPreference);
+
 
 module.exports=customersRouting;
